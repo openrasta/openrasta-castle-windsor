9d60f2ba064e76bc085ec6864f056e3bcb085759
Sebastien Lambla <seb@serialseb.com>
2018-04-13 12:10:00 +0200

    Change init order to maybe get windows to stop moaning

--- src/OpenRasta.DI.Windsor.Tests.Unit/WindsorWithHttpListenerHost_Specification.cs
+++ src/OpenRasta.DI.Windsor.Tests.Unit/WindsorWithHttpListenerHost_Specification.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Net;
+using System.Runtime.InteropServices;
 using Castle.MicroKernel.Registration;
 using Castle.Windsor;
 using NUnit.Framework;
+using NUnit.Framework.Internal;
 using OpenRasta.Codecs;
 using OpenRasta.Configuration;
 using OpenRasta.DI;
@@ -13,149 +15,154 @@ using OpenRasta.Web;
 
 namespace WindsorWithHttpListenerHost_Specification
 {
-    public class TestHttpListenerHostWithConfig : HttpListenerHost
+  public class TestHttpListenerHostWithConfig : HttpListenerHost
+  {
+    private readonly IConfigurationSource _source;
+    public IDependencyResolver Resolver { get; private set; }
+
+    public TestHttpListenerHostWithConfig(IConfigurationSource source)
     {
-        private readonly IConfigurationSource _source;
-        public IDependencyResolver Resolver { get; private set; }
-
-        public TestHttpListenerHostWithConfig(IConfigurationSource source)
-        {
-            _source = source ?? throw new ArgumentNullException(nameof(source));
-        }
-
-        public override bool ConfigureRootDependencies(IDependencyResolver resolver)
-        {
-            var configured = base.ConfigureRootDependencies(resolver);
-            if (configured && _source != null)
-            {
-                resolver.AddDependencyInstance<IConfigurationSource>(_source);
-            }
-            Resolver = resolver;
-            return configured;
-        }
+      _source = source ?? throw new ArgumentNullException(nameof(source));
     }
 
-    class TestConfigurationSource : IConfigurationSource
+    public override bool ConfigureRootDependencies(IDependencyResolver resolver)
     {
-        public void Configure()
-        {
-                ResourceSpace.Has.ResourcesOfType<string>()
-                    .AtUri("/").Named("Root")
-                    .And.AtUri("/with-header").Named("WithHeader")
-                    .HandledBy<TestHandler>()
-                    .And.HandledBy<HeaderSettingHandler>()
-                    .TranscodedBy<TextPlainCodec>();
-        }
+      var configured = base.ConfigureRootDependencies(resolver);
+      if (configured && _source != null)
+      {
+        resolver.AddDependencyInstance<IConfigurationSource>(_source);
+      }
+
+      Resolver = resolver;
+      return configured;
+    }
+  }
+
+  class TestConfigurationSource : IConfigurationSource
+  {
+    public void Configure()
+    {
+      ResourceSpace.Has.ResourcesOfType<string>()
+        .AtUri("/").Named("Root")
+        .And.AtUri("/with-header").Named("WithHeader")
+        .HandledBy<TestHandler>()
+        .And.HandledBy<HeaderSettingHandler>()
+        .TranscodedBy<TextPlainCodec>();
+    }
+  }
+
+  public class TestHandler
+  {
+    [HttpOperation(ForUriName = "Root")]
+    public string Get()
+    {
+      return "Test Root Response";
+    }
+  }
+
+  public class HeaderSettingHandler
+  {
+    readonly IResponse _response;
+
+    public HeaderSettingHandler(IResponse response)
+    {
+      _response = response;
     }
 
-    public class TestHandler
+    [HttpOperation(ForUriName = "WithHeader")]
+    public string Get()
     {
-        [HttpOperation(ForUriName = "Root")]
-        public string Get()
-        {
-            return "Test Root Response";
-        }
+      _response.Headers["FOO"] = "BAR";
+      return "Test Header Response";
+    }
+  }
+
+  public class WindsorDependencyResolverAccessor : IDependencyResolverAccessor
+  {
+    private static IWindsorContainer _container;
+
+    public IDependencyResolver Resolver { get; private set; }
+
+    public WindsorDependencyResolverAccessor()
+    {
+      Resolver = new WindsorDependencyResolver(_container);
     }
 
-    public class HeaderSettingHandler
+    public static void SetupWith(IWindsorContainer container)
     {
-        readonly IResponse _response;
+      if (container == null)
+        throw new ArgumentNullException("container");
 
-        public HeaderSettingHandler(IResponse response)
-        {
-            _response = response;
-        }
+      if (_container == null)
+        _container = container;
+    }
+  }
 
-        [HttpOperation(ForUriName = "WithHeader")]
-        public string Get()
-        {
-            _response.Headers["FOO"] = "BAR";
-            return "Test Header Response";
-        }
+  public class when_creating_a_new_HttpListenerHost_with_WindsorResolver : IDisposable
+  {
+    static Random randomPort = new Random();
+    TestHttpListenerHostWithConfig _host;
+    string Prefix;
+    private IWindsorContainer _container;
+
+    public when_creating_a_new_HttpListenerHost_with_WindsorResolver()
+    {
+      // init container
+      _container = new WindsorContainer();
+      var port = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? 18981 : randomPort.Next(1024, 2048);
+      
+      Prefix = $"http://localhost:{port}/";
+
+      _container.Register(
+        Component.For<IConfigurationSource>().ImplementedBy<TestConfigurationSource>().LifestyleSingleton(),
+        Component.For<TestHttpListenerHostWithConfig>().ImplementedBy<TestHttpListenerHostWithConfig>()
+          .LifestyleSingleton(),
+        Component.For<IRequest>().UsingFactoryMethod(() => (IRequest) null).LifestyleScoped(),
+        Component.For<IResponse>().UsingFactoryMethod(() => (IResponse) null).LifestyleScoped(),
+        Component.For<ICommunicationContext>().UsingFactoryMethod(() => (ICommunicationContext) null)
+          .LifestyleScoped());
+
+      // Statically set the container for the resolver accessor type as it is created by 
+      // Activator.CreateInstance(Type type) in HttpListenerHost.Initialize(...)
+      WindsorDependencyResolverAccessor.SetupWith(_container);
+
+      var resolverFactoryType = typeof(WindsorDependencyResolverAccessor);
+
+      _host = _container.Resolve<TestHttpListenerHostWithConfig>();
+      _host.Initialize(new[] {Prefix}, "/", resolverFactoryType);
+      _host.StartListening();
     }
 
-    public class WindsorDependencyResolverAccessor : IDependencyResolverAccessor
+
+    public void Dispose()
     {
-        private static IWindsorContainer _container;
-
-        public IDependencyResolver Resolver { get; private set; }
-
-        public WindsorDependencyResolverAccessor()
-        {
-            Resolver = new WindsorDependencyResolver(_container);
-        }
-
-        public static void SetupWith(IWindsorContainer container)
-        {
-            if (container == null)
-                throw new ArgumentNullException("container");
-
-            if (_container == null)
-                _container = container;
-        }
+      _host.StopListening();
+      _host.Close();
     }
 
-    public class when_creating_a_new_HttpListenerHost_with_WindsorResolver : context
+    [Test]
+    public void the_resolver_is_a_windsor_dependency_resolver()
     {
-        static Random randomPort = new Random();
-        TestHttpListenerHostWithConfig _host;
-        string Prefix;
-        private IWindsorContainer _container;
+      Assert.That(_host.Resolver, Is.Not.Null);
+      Assert.That(_host.Resolver, Is.InstanceOf<WindsorDependencyResolver>());
+    }
 
-        protected override void SetUp()
-        {
-            // init container
-            _container = new WindsorContainer();
-            Prefix = $"http://localhost:{randomPort.Next(1024, 2048)}/";
+    [Test]
+    public void the_root_uri_serves_the_test_string()
+    {
+      var response = new WebClient().DownloadString(Prefix);
+      Assert.That(response, Is.EqualTo("Test Root Response"));
+    }
 
-            _container.Register(
-                Component.For<IConfigurationSource>().ImplementedBy<TestConfigurationSource>().LifestyleSingleton(),
-                Component.For<TestHttpListenerHostWithConfig>().ImplementedBy<TestHttpListenerHostWithConfig>().LifestyleSingleton(),
-                
-                Component.For<IRequest>().UsingFactoryMethod(() => (IRequest)null).LifestyleScoped(),
-                Component.For<IResponse>().UsingFactoryMethod(() => (IResponse)null).LifestyleScoped(),
-            Component.For<ICommunicationContext>().UsingFactoryMethod(() => (ICommunicationContext)null).LifestyleScoped());
+    [Test]
+    public void the_with_header_uri_serves_the_response_header()
+    {
+      var webClient = new WebClient();
+      var response = webClient.DownloadString(Prefix + "with-header");
+      Assert.That(response, Is.EqualTo("Test Header Response"));
 
-            // Statically set the container for the resolver accessor type as it is created by 
-            // Activator.CreateInstance(Type type) in HttpListenerHost.Initialize(...)
-            WindsorDependencyResolverAccessor.SetupWith(_container);
-
-            var resolverFactoryType = typeof(WindsorDependencyResolverAccessor);
-
-            _host = _container.Resolve<TestHttpListenerHostWithConfig>();
-            _host.Initialize(new[] { Prefix }, "/", resolverFactoryType);
-            _host.StartListening();
-        }
-
-        protected override void TearDown()
-        {
-            _host.StopListening();
-            _host.Close();
-        }
-
-        [Test]
-        public void the_resolver_is_a_windsor_dependency_resolver()
-        {
-            Assert.That(_host.Resolver, Is.Not.Null);
-            Assert.That(_host.Resolver, Is.InstanceOf<WindsorDependencyResolver>());
-        }
-
-        [Test]
-        public void the_root_uri_serves_the_test_string()
-        {
-            var response = new WebClient().DownloadString(Prefix);
-            Assert.That(response, Is.EqualTo("Test Root Response"));
-        }
-
-        [Test]
-        public void the_with_header_uri_serves_the_response_header()
-        {
-            var webClient = new WebClient();
-            var response = webClient.DownloadString(Prefix + "with-header");
-            Assert.That(response, Is.EqualTo("Test Header Response"));
-
-            var fooHeader = webClient.ResponseHeaders["FOO"];
-            Assert.That(fooHeader, Is.EqualTo("BAR"));
-        }
-    } 
+      var fooHeader = webClient.ResponseHeaders["FOO"];
+      Assert.That(fooHeader, Is.EqualTo("BAR"));
+    }
+  }
 }
